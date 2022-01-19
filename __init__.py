00e5bfcf35a72f1077bc5cc91ab61d2ce38815bf
Momoka <79566882+obrmmk@users.noreply.github.com>
2022-01-19 15:21:59 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -204,10 +204,10 @@ def start_BASE_corgi(model_id='1ejyg2VzwA-MbaXANmLALYBUfV4iD3_W1', delay=600, pr
     nmt = compose_nmt(generate_nmt(model_id=model_id))
     run_corgi(nmt, delay=delay, print=print)
     
-def start_BASE_exp_corgi(model_id='1B6yh8T9FkfPnsjF2NPM5uU5dljnYrH5Q', delay=600, print=print_nop):#211229
+def start_BASE_expt_corgi(model_id='1B6yh8T9FkfPnsjF2NPM5uU5dljnYrH5Q', delay=600, print=print_nop):#211229
     nmt = compose_nmt(generate_nmt(model_id=model_id))
     run_corgi(nmt, delay=delay, print=print)
     
-def start_tuned_by_exp_corgi(model_id='1yEFafM63OvxwZOzgp1_cgBnlKmBC-Py8', delay=600, print=print_nop):#211229
+def start_tuned_by_expt_corgi(model_id='1yEFafM63OvxwZOzgp1_cgBnlKmBC-Py8', delay=600, print=print_nop):#211229
     nmt = compose_nmt(generate_nmt(model_id=model_id))
     run_corgi(nmt, delay=delay, print=print)
